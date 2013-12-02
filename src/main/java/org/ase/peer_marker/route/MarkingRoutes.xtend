4ba1aeeaa8183968ea82da796c316e86c647eac2
toby <toby@toby-MacBookAir>
2013-12-02 11:59:16 +0200

    Using JsonTransformer instead of manual JSON generation

--- src/main/java/org/ase/peer_marker/route/MarkingRoutes.xtend
+++ src/main/java/org/ase/peer_marker/route/MarkingRoutes.xtend
@@ -1,15 +1,16 @@
 package org.ase.peer_marker.route
 
-import org.ase.peer_marker.route.BaseRoute
+import org.ase.peer_marker.transformer.JsonTransformer
 
 class MarkingRoutes extends BaseRoute {
 
    override load() {
-      get("/api/marking") [ req, res |
-         res.type("application/json")
-         '''[{"answers": 5, "evaluations": 0}, {"answers": 1, "evaluations": 1} ]'''
-      ]
-
+      get(new JsonTransformer("/api/marking") [ req, res |
+         #[
+            #{"answers" -> 5, "evaluations" -> 0}, 
+            #{"answers" -> 1, "evaluations" -> 1}
+         ]
+      ])
    }
 
 }
--- src/main/xtend-gen/org/ase/peer_marker/route/MarkingRoutes.java
+++ src/main/xtend-gen/org/ase/peer_marker/route/MarkingRoutes.java
@@ -1,8 +1,13 @@
 package org.ase.peer_marker.route;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.tobykurien.sparkler.Sparkler;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import org.ase.peer_marker.route.BaseRoute;
-import org.eclipse.xtend2.lib.StringConcatenation;
+import org.ase.peer_marker.transformer.JsonTransformer;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import spark.Request;
 import spark.Response;
@@ -10,18 +15,22 @@ import spark.Response;
 @SuppressWarnings("all")
 public class MarkingRoutes extends BaseRoute {
   public void load() {
-    final Function2<Request,Response,String> _function = new Function2<Request,Response,String>() {
-      public String apply(final Request req, final Response res) {
-        String _xblockexpression = null;
-        {
-          res.type("application/json");
-          StringConcatenation _builder = new StringConcatenation();
-          _builder.append("[{\"answers\": 5, \"evaluations\": 0}, {\"answers\": 1, \"evaluations\": 1} ]");
-          _xblockexpression = (_builder.toString());
-        }
-        return _xblockexpression;
+    final Function2<Request,Response,List<Map<String,Integer>>> _function = new Function2<Request,Response,List<Map<String,Integer>>>() {
+      public List<Map<String,Integer>> apply(final Request req, final Response res) {
+        Map<String,Integer> _xsetliteral = null;
+        Map<String,Integer> _tempMap = Maps.<String, Integer>newHashMap();
+        _tempMap.put("answers", Integer.valueOf(5));
+        _tempMap.put("evaluations", Integer.valueOf(0));
+        _xsetliteral = Collections.<String, Integer>unmodifiableMap(_tempMap);
+        Map<String,Integer> _xsetliteral_1 = null;
+        Map<String,Integer> _tempMap_1 = Maps.<String, Integer>newHashMap();
+        _tempMap_1.put("answers", Integer.valueOf(1));
+        _tempMap_1.put("evaluations", Integer.valueOf(1));
+        _xsetliteral_1 = Collections.<String, Integer>unmodifiableMap(_tempMap_1);
+        return Collections.<Map<String, Integer>>unmodifiableList(Lists.<Map<String, Integer>>newArrayList(_xsetliteral, _xsetliteral_1));
       }
     };
-    Sparkler.get("/api/marking", _function);
+    JsonTransformer _jsonTransformer = new JsonTransformer("/api/marking", _function);
+    Sparkler.get(_jsonTransformer);
   }
 }
