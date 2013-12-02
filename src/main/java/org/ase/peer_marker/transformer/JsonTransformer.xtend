39cd6461dffd45ea2a94e583a2387b03b3d9c58f
toby <toby@toby-MacBookAir>
2013-12-02 10:45:51 +0200

    moved all the code from JsonTranformer into sparkler for release v0.0.2

--- src/main/java/org/ase/peer_marker/transformer/JsonTransformer.xtend
+++ src/main/java/org/ase/peer_marker/transformer/JsonTransformer.xtend
@@ -13,50 +13,10 @@ import com.fasterxml.jackson.databind.ObjectMapper
 /**
  * Returns a JSON serialized version of Model objects
  */
-class JsonTransformer extends ResponseTransformerRoute {
-   var (Request, Response)=>Object handler
-
+class JsonTransformer extends com.tobykurien.sparkler.transformer.JsonTransformer {
+   
    new(String path, (Request, Response)=>Object handler) {
-      super(path, "application/json")
-      this.handler = handler
+      super(path, handler)
    }
-
-   override render(Object json) {
-      return json.toString
-   }
-
-   def escapeString(String s) {
-      s.replace("\"", "\\\"")
-   }
-
-   override handle(Request request, Response response) {
-      response.type("application/json")
-
-      try {
-         Base.open(DatabaseManager.newDataSource)
-         var model = handler.apply(request, response)
-
-         if (model == null) {
-            response.status(404)
-            "{'error': 'Object not found'}"
-         } else {
-            if (model instanceof Model) {
-               return (model as Model).toJson(false)
-            } else if (model instanceof LazyList) {
-               return (model as LazyList).toJson(false)
-            } else if (model == null) {
-               null
-            } else {
-            	new ObjectMapper().writeValueAsString(model);
-            }
-         }
-      } catch (Exception e) {
-         response.status(500)
-         var error = Helper.handleError(request, response, e)
-         System.err.println(error)
-         "{\"error\" : \""+ error.escapeString + "\"}"
-      } finally {
-         Base.close()
-      }
-   }
-}  
+   
+}
--- src/main/xtend-gen/org/ase/peer_marker/transformer/JsonTransformer.java
+++ src/main/xtend-gen/org/ase/peer_marker/transformer/JsonTransformer.java
@@ -1,109 +1,15 @@
 package org.ase.peer_marker.transformer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Objects;
-import com.tobykurien.sparkler.Helper;
-import com.tobykurien.sparkler.db.DatabaseManager;
-import javax.sql.DataSource;
-import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
-import org.javalite.activejdbc.Base;
-import org.javalite.activejdbc.LazyList;
-import org.javalite.activejdbc.Model;
 import spark.Request;
 import spark.Response;
-import spark.ResponseTransformerRoute;
 
 /**
  * Returns a JSON serialized version of Model objects
  */
 @SuppressWarnings("all")
-public class JsonTransformer extends ResponseTransformerRoute {
-  private Function2<? super Request,? super Response,? extends Object> handler;
-  
+public class JsonTransformer extends com.tobykurien.sparkler.transformer.JsonTransformer {
   public JsonTransformer(final String path, final Function2<? super Request,? super Response,? extends Object> handler) {
-    super(path, "application/json");
-    this.handler = handler;
-  }
-  
-  public String render(final Object json) {
-    return json.toString();
-  }
-  
-  public String escapeString(final String s) {
-    String _replace = s.replace("\"", "\\\"");
-    return _replace;
-  }
-  
-  public Object handle(final Request request, final Response response) {
-    String _xblockexpression = null;
-    {
-      response.type("application/json");
-      String _xtrycatchfinallyexpression = null;
-      try {
-        String _xblockexpression_1 = null;
-        {
-          DataSource _newDataSource = DatabaseManager.newDataSource();
-          Base.open(_newDataSource);
-          Object model = this.handler.apply(request, response);
-          String _xifexpression = null;
-          boolean _equals = Objects.equal(model, null);
-          if (_equals) {
-            String _xblockexpression_2 = null;
-            {
-              response.status(404);
-              _xblockexpression_2 = ("{\'error\': \'Object not found\'}");
-            }
-            _xifexpression = _xblockexpression_2;
-          } else {
-            String _xifexpression_1 = null;
-            if ((model instanceof Model)) {
-              return ((Model) model).toJson(false);
-            } else {
-              String _xifexpression_2 = null;
-              if ((model instanceof LazyList)) {
-                return ((LazyList) model).toJson(false);
-              } else {
-                String _xifexpression_3 = null;
-                boolean _equals_1 = Objects.equal(model, null);
-                if (_equals_1) {
-                  _xifexpression_3 = null;
-                } else {
-                  ObjectMapper _objectMapper = new ObjectMapper();
-                  String _writeValueAsString = _objectMapper.writeValueAsString(model);
-                  _xifexpression_3 = _writeValueAsString;
-                }
-                _xifexpression_2 = _xifexpression_3;
-              }
-              _xifexpression_1 = _xifexpression_2;
-            }
-            _xifexpression = _xifexpression_1;
-          }
-          _xblockexpression_1 = (_xifexpression);
-        }
-        _xtrycatchfinallyexpression = _xblockexpression_1;
-      } catch (final Throwable _t) {
-        if (_t instanceof Exception) {
-          final Exception e = (Exception)_t;
-          String _xblockexpression_2 = null;
-          {
-            response.status(500);
-            String error = Helper.handleError(request, response, e);
-            System.err.println(error);
-            String _escapeString = this.escapeString(error);
-            String _plus = ("{\"error\" : \"" + _escapeString);
-            String _plus_1 = (_plus + "\"}");
-            _xblockexpression_2 = (_plus_1);
-          }
-          _xtrycatchfinallyexpression = _xblockexpression_2;
-        } else {
-          throw Exceptions.sneakyThrow(_t);
-        }
-      } finally {
-        Base.close();
-      }
-      _xblockexpression = (_xtrycatchfinallyexpression);
-    }
-    return _xblockexpression;
+    super(path, handler);
   }
 }
