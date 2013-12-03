e3f45e7f4339e132e7e414bea4b032730b859f29
toby <toby@toby-MacBookAir>
2013-12-03 14:55:45 +0200

    remove xtend gen files from commit

--- src/main/xtend-gen/org/ase/peer_marker/model/Evaluation.java
+++
@@ -1,7 +0,0 @@
-package org.ase.peer_marker.model;
-
-import org.javalite.activejdbc.Model;
-
-@SuppressWarnings("all")
-public class Evaluation extends Model {
-}
--- src/main/xtend-gen/org/ase/peer_marker/route/BaseRoute.java
+++
@@ -1,8 +0,0 @@
-package org.ase.peer_marker.route;
-
-import com.tobykurien.sparkler.Sparkler;
-
-@SuppressWarnings("all")
-public abstract class BaseRoute extends Sparkler {
-  public abstract void load();
-}
