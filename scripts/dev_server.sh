f9943cb9b6de44814c96bc480a4dd5355dab461c
Toby Kurien <toby@tobykurien.com>
2017-03-14 14:11:54 +0200

    updated sparkler and build for gradle, added button for student marking

--- scripts/dev_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-BASEDIR=$(dirname $0)
-
-cd $BASEDIR/..
-
-java -cp bin:libs/* -Denvironment=development org.ase.peer_marker.Main
--- scripts/server.sh
+++ scripts/server.sh
@@ -3,4 +3,4 @@ BASEDIR=$(dirname $0)
 
 cd $BASEDIR/..
 
-java -cp bin:libs/* -Denvironment=production org.ase.peer_marker.Main
+java -Denvironment=$SPARKLER_ENV -cp build/classes/main:build/libs/* org.ase.peer_marker.Main
